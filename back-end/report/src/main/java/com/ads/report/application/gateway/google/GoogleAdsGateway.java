96b7908a1486a450c0c6b6f260f78c4df2be57cc
Marcus-Nastasi <vinnie.nsts@gmail.com>
2025-01-15 21:53:49 -0300

    feat: includes javadoc comments on GoogleAdsGateway (interface).

--- back-end/report/src/main/java/com/ads/report/application/gateway/google/GoogleAdsGateway.java
+++ back-end/report/src/main/java/com/ads/report/application/gateway/google/GoogleAdsGateway.java
@@ -6,13 +6,57 @@ import com.ads.report.domain.metrics.AccountMetrics;
 
 import java.util.List;
 
+/**
+ * The interface of the google ads api calls.
+ *
+ * <p>
+ * This represents the interface that communicates the application with the infra layer,
+ * making possible to make api calls.
+ * <p/>
+ *
+ * @author Marcus Nastasi
+ * @version 1.0.1
+ * @since 2025
+ * */
 public interface GoogleAdsGateway {
 
+    /**
+     * Test the connection with the adwords client.
+     *
+     * @return The status and a list of accessible customer accounts.
+     * @throws RuntimeException If fails to connect.
+     * */
     List<String> testConnection();
 
+    /**
+     * Get campaigns and it's metrics.
+     *
+     * @param customerId The id of an adwords customer (client).
+     *
+     * @return The status and a list of CampaignMetrics objects.
+     * @throws RuntimeException If fails to request the data.
+     */
     List<CampaignMetrics> getCampaignMetrics(String customerId);
 
+    /**
+     * Get general information of manager account.
+     *
+     * @param managerAccountId The id of an adwords customer (client).
+     *
+     * @return A ManagerAccountInfo type object.
+     * @throws RuntimeException If fails to request the data.
+     */
     ManagerAccountInfo getManagerAccount(String managerAccountId);
 
+    /**
+     * Get general information of manager account.
+     *
+     * @param customerId The id of an adwords customer (client).
+     * @param startDate The start date of the analysis period.
+     * @param endDate The end date of the analysis period.
+     *
+     * @return A ManagerAccountInfo type object.
+     * @throws RuntimeException If fails to request the data.
+     */
     List<AccountMetrics> getAccountMetrics(String customerId, String startDate, String endDate);
 }
